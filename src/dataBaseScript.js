d789d40fd577b9aacc9af3c6cc179f8469ad057d
u1974750 <99675264+u1974750@users.noreply.github.com>
2023-05-28 17:17:42 +0200

    a

--- src/dataBaseScript.js
+++ src/dataBaseScript.js
@@ -15,7 +15,7 @@ createList().then(function(){
         document.getElementById("ImatgeItem"+i).src = r.imatge;
         nomItem = document.getElementById("NomItem"+i).innerHTML = r.Name;
         document.getElementById("DescrpcioItem"+i).innerText = r.Description;
-        document.getElementById("PreuItem"+i).innerText = r.Price;
+        document.getElementById("PreuItem"+i).innerText = r.Price + "gold";
         document.getElementById("EstacioItem"+i).innerText = r.Season;
         i+=1;
     }
