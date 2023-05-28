5094c0d1a8def0d880a928d829b524c392881d58
u1974750 <99675264+u1974750@users.noreply.github.com>
2023-05-28 12:35:03 +0200

    Update dataBaseScript.js

--- src/dataBaseScript.js
+++ src/dataBaseScript.js
@@ -12,18 +12,20 @@ async function createList() {
 createList().then(function(){
     console.table(data.value);
     console.log(data.value.Name);
-    
-    var i = 1
-    for (let r of data.value) {
-        // console.log(r.imatge);
-        // document.getElementById("ImatgeItem"+i).src = r.imatge;
-        // console.log("ImatgeItem"+i);
-        console.log("NomItem"+i);
-        console.log(r.Name);
-        console.log(document.getElementById("NomItem1").innerHTML);
-        nomItem = document.getElementById("NomItem1").innerHTML = r.Name;
-        document.getElementById("DescrpcioItem"+i).innerText = r.Description;
-        document.getElementById("PreuItem"+i).innerText = r.Price;
-        console.log(r.Name);
-    }
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        //Do work
+        var i = 1
+        for (let r of data.value) {
+            // console.log(r.imatge);
+            // document.getElementById("ImatgeItem"+i).src = r.imatge;
+            // console.log("ImatgeItem"+i);
+            console.log("NomItem"+i);
+            console.log(r.Name);
+            console.log(document.getElementById("NomItem1").innerHTML);
+            nomItem = document.getElementById("NomItem1").innerHTML = r.Name;
+            document.getElementById("DescrpcioItem"+i).innerText = r.Description;
+            document.getElementById("PreuItem"+i).innerText = r.Price;
+            console.log(r.Name);
+        }
+    });
 });
