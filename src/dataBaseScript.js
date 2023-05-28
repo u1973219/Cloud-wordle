f455c75b0c3e9fb330ddf831009279e99aa3bc05
u1974750 <99675264+u1974750@users.noreply.github.com>
2023-05-28 11:58:52 +0200

    Update dataBaseScript.js

--- src/dataBaseScript.js
+++ src/dataBaseScript.js
@@ -13,7 +13,10 @@ createList().then(function(){
     console.table(data.value);
     console.log(data.value.Name);
     
+    var i = 1
     for (let r of data.value) {
+        document.getElementById("ImatgeItem"+i).src = r.imatge;
+        document.getElementById("NomItem"+i).innerHTML = r.Name;
         console.log(r.Name);
     }
 });
