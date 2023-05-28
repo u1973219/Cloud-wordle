b3c60cd1e1aefce6e969b007e7a8085ac78f7b76
u1973219 <u1973219@campus.udg.edu>
2023-05-28 19:21:07 +0200

    a
    a

--- src/translator.js
+++ src/translator.js
@@ -1,30 +1,28 @@
-function traduirText(){
+function traduirText() {
     var textInput = document.getElementById("text-to-translate").value;
     var apiKey = "948738bc08c14cb9b5af370695204fc6";
     var location = "westeurope";
-    var endpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.@$to=es";
-
-
+    var endpoint = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es";
+  
     var req = new XMLHttpRequest();
-    req.open("POST",endpoint,true);
-    req.setRequestHeader("Content-Type","application/json");
+    req.open("POST", endpoint, true);
+    req.setRequestHeader("Content-Type", "application/json");
     req.setRequestHeader("Ocp-Apim-Subscription-Key", apiKey);
-    req.setRequestHeader("Ocp-Apim-Subscription-Region",location);
-    
-    if(req.readyState === 4 && req.status === 200){
-        var resposta = JSON.parse(req.responseText);
-        var resultatTraduit = response[0].translations[0].text;
-        document.getElementById("text-aplicat").innerHTML = resultatTraduit;
-        console.log(resultatTraduit);
-        var data = JSON.stringify([{ "Text": textInput}]);
-        req.send(data);
-    }
-
-
-
-
-
-
-
-
-}
+    req.setRequestHeader("Ocp-Apim-Subscription-Region", location);
+  
+    req.onreadystatechange = function () {
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+          var resposta = JSON.parse(req.responseText);
+          var resultatTraduit = resposta[0].translations[0].text;
+          document.getElementById("text-aplicat").innerHTML = resultatTraduit;
+          console.log(resultatTraduit);
+        } else {
+          console.error("Error en la solicitud AJAX:", req.status);
+        }
+      }
+    };
+  
+    var data = JSON.stringify([{ "Text": textInput }]);
+    req.send(data);
+  }
