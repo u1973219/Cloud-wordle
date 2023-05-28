85d2fbfc31449ff86020ce1a5272d8c634955a46
u1974750 <99675264+u1974750@users.noreply.github.com>
2023-05-28 12:50:20 +0200

    a

--- src/dataBaseScript.js
+++ src/dataBaseScript.js
@@ -5,23 +5,18 @@ async function createList() {
     const endpoint = '/data-api/rest/Person';
     const response = await fetch(endpoint);
     data = await response.json();
-    console.table(data.value);
 }
 
 
 createList().then(function(){
     console.table(data.value);
-    console.log(data.value.Name);    
     var i = 1
     for (let r of data.value) {
-        console.log(document.getElementById("NomItem1").innerHTML);
-
         document.getElementById("ImatgeItem"+i).src = r.imatge;
         nomItem = document.getElementById("NomItem"+i).innerHTML = r.Name;
         document.getElementById("DescrpcioItem"+i).innerText = r.Description;
         document.getElementById("PreuItem"+i).innerText = r.Price;
         document.getElementById("EstacioItem"+i).innerText = r.Season;
-        console.log(r.Name);
         i+=1;
     }
 });
