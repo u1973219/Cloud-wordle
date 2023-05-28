ef907b14d15d4fb81f46cf4944efcf92eea483f6
u1974750 <99675264+u1974750@users.noreply.github.com>
2023-05-28 11:36:59 +0200

    Update dataBaseScript.js

--- src/dataBaseScript.js
+++ src/dataBaseScript.js
@@ -1,9 +1,10 @@
 
+var data;
 
 async function createList() {
     const endpoint = '/data-api/rest/Person';
     const response = await fetch(endpoint);
-    var data = await response.json();
+    data = await response.json();
     console.table(data.value);
 }
 
